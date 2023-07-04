d51bd4ad5916ffb515514d7e0644f63f5c39c013
karthik61 <karthik@aspectsites.com>
2023-07-04 16:45:18 +0530

    fix

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const db = require('./database/database');
 const router = require('./router/router');
 var bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(cors())
-
-
-
 app.use('/api', router)
+
+app.use(express.static(path.join(__dirname, 'admin', 'dist')));
+app.get('*/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'admin', 'dist', 'index.html'));
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
